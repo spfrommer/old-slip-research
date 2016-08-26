5f0bc8fa369a338efc145cecd7c6c49f951ea869
Samuel Pfrommer <sam.pfrommer@gmail.com>
2016-08-26 19:06:12 -0400

    Added pertubation function for slightly changing optimal solution

---
+++ slipping_dynamics/perturb.m
@@ -0,0 +1,9 @@
+function [ perturbed ] = perturb( funparams, sp )
+% Adds small random changes to the hip torques and actuated length ddots
+[ phaseT, xtoe, xtoedot, x, xdot, y, ydot, ...
+           ra, radot, raddot, torque] = unpack(funparams, sp);
+perturbed = [phaseT; xtoe; xtoedot; x; xdot; y; ydot; ra; radot;
+             raddot + (rand(sp.gridn, 1) - 0.5) * 0.001;
+             torque + (rand(sp.gridn, 1) - 0.5) * 0.001];
+end
+
