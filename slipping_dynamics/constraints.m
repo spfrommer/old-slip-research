dd968beb0671a68f0dd60b1488b79ad84341c73e
Samuel Pfrommer <sam.pfrommer@gmail.com>
2016-08-29 14:42:36 -0400

    Modified end constraints to include x position

--- slipping_dynamics/constraints.m
+++ slipping_dynamics/constraints.m
@@ -87,5 +87,6 @@ function [ c, ceq ] = constraints( funparams, sp )
     % Add first phase start constraints
     ceq = [ceq; xtoe(1)-0.5; xtoedot(1); x(1)-0.5; xdot(1); ydot(1); ra(1) - 1; radot(1)];
     % Add lastphase end constraints
-    ceq = [ceq; xtoe(end)-1.1];
+    ceq = [ceq; xtoe(end)-(sp.slipPatch(2) + 0.1); ...
+                x(end)-(sp.slipPatch(2) + 0.1)];
 end
