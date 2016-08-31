61049188cb53f80cf59268eacc87bde962b94a76
Samuel Pfrommer <sam.pfrommer@gmail.com>
2016-08-30 21:05:02 -0400

    Implemented iterative optimizing

--- slipping_dynamics/main.m
+++ slipping_dynamics/main.m
@@ -5,12 +5,8 @@ tic
 % gridn actuated lengths, gridn actlengthdots, gridn actlengthddots (input),
 % gridn leg angles, gridn leg angle dots, gridn hip torques, gridn toe x,
 % gridn toe xdot
-if exist('optimal','var')
-    disp('Using previous solution as starting guess...');
-    x0 = optimal;
-else
-    x0 = ones(length(sp.phases)+sp.gridn*length(sp.phases)*10,1);
-end
+x0 = ones(length(sp.phases)+sp.gridn*length(sp.phases)*10,1) * 1;
+
 % No linear inequality or equality constraints
 A = [];
 b = [];
@@ -25,8 +21,34 @@ options = optimoptions(@fmincon, 'TolFun', 0.00000001, ...
                        'Display', 'iter', 'Algorithm', 'sqp', ...
                        'StepTolerance', 1e-13);
 
-% Solve the optimization problem
-optimal = fmincon(@(x) constcost(x, sp), x0, A, b, Aeq, Beq, lb, ub, ...
-                  @(x) constraints(x, sp), options);
-disp(sprintf('Finished in %f seconds', toc));
+% First find any feasible trajectory
+[optimal,~,flag,~] = fmincon(@(x) constcost(x,sp),x0,A,b,Aeq,Beq,lb,ub, ...
+                             @(x) constraints(x,sp), options);
+if flag ~= 1
+    disp('No feasible trajectory found!');
+else
+    disp('Found feasible trajectory, optimizing...');
+    lastTime = timecost(optimal, sp);
+    fprintf('Starting trajectory has time of: %f\n', lastTime);
+    lastOpt = optimal;
+    while true
+        % Optimize the trajectory with respect to time
+        [optimal,time,flag,~] = fmincon(@(x) timecost(x,sp),optimal,A,b,...
+                 Aeq, Beq, lb, ub, @(x) constraints(x, sp), options);
+        if flag <= 0
+            fprintf('Optimizer did not find local minimum, exiting with flag: %d\n', flag);
+            break;
+        end
+        if lastTime - time < 1e-7
+            disp('Optimizer returned no benefit, exiting');
+            break;
+        end
+        lastTime = time;
+        lastOpt = optimal;
+        fprintf('Found better trajectory with time: %f\n', time);
+    end
+    optimal = lastOpt;
+    fprintf('Best time: %f\n', lastTime);
+end
+fprintf('Finished in %f seconds\n', toc);
 visualize
