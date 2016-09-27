c13118b0396f3338dd0a4b58614110aecefae48a
Samuel Pfrommer <sam.pfrommer@gmail.com>
2016-09-27 17:42:28 -0400

    Added automation of multiple starting points to avoid local minima
    This is still not working, it's falling into local minima like crazy, whether with
    actuation costs or time costs. I'll try to see if I can first replicate mutliple
    steps on flat terrain.

--- slipping_dynamics/actcost.m
+++ slipping_dynamics/actcost.m
@@ -1,8 +1,8 @@
 function [ cost ] = actcost( funparams, sp )
     % Unpack the vector
-    [phaseT, xtoe, xtoedot, x, xdot, y, ydot, ra, radot, raddot, hiptorque] = ...
+    [~, ~, ~, ~, ~, ~, ~, ~, ~, raddot, hiptorque] = ...
         unpack(funparams, sp);
         
-    cost = sum(hiptorque.^2) + sum(raddot.^2);
+    cost = sum((hiptorque./10).^2) + sum(raddot.^2);
 end
 
