e8eb7d5fd63165d47a91e0cfcaad765127e878e1
Samuel Pfrommer <sam.pfrommer@gmail.com>
2016-10-17 21:05:59 -0400

    Changed actcost function to reflect actuator work

--- slipping_dynamics/actcost.m
+++ slipping_dynamics/actcost.m
@@ -1,8 +1,19 @@
 function [ cost ] = actcost( funparams, sp )
     % Unpack the vector
-    [~, ~, ~, ~, ~, ~, ~, ~, ~, raddot] = ...
+    [stanceT, ~, xtoe, x, xdot, y, ydot, ra, radot, ~] = ...
         unpack(funparams, sp);
-        
-    cost = sum(raddot.^2);
+    
+    r = sqrt((x - xtoe).^2 + y.^2);
+    rdot = ((x-xtoe).*(xdot)+y.*ydot)./(r);
+    fs = sp.spring * (ra - r) + sp.damp * (radot - rdot);
+
+    epsilon = 0.001;
+    work = sqrt((fs.*radot).^2 + epsilon^2) - epsilon;    
+    
+    cost = 0;
+    for i = 1 : size(sp.phases, 1);
+        dt = stanceT(i) / sp.gridn;
+        cost = cost + sum(work(sp.gridn * (i-1) + 1 : sp.gridn * i) * dt);
+    end
 end
 
