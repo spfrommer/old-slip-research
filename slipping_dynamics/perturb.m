e280eb0e504e7aef6357b2a4ebf6e34e3efc4454
Samuel Pfrommer <sam.pfrommer@gmail.com>
2016-10-24 20:22:26 -0400

    Changed perturb function to affect all variables

--- slipping_dynamics/perturb.m
+++ slipping_dynamics/perturb.m
@@ -1,8 +1,4 @@
-function [ perturbed ] = perturb( funparams, sp )
-% Adds small random changes to the hip torques and actuated length ddots
-[ stanceT, flightT, xtoe, x, xdot, y, ydot, ...
-           ra, radot, raddot] = unpack(funparams, sp);
-perturbed = [stanceT; flightT; xtoe; x; xdot; y; ydot; ra; radot;
-             raddot + (rand(sp.gridn*length(sp.phases), 1) - 0.5) * 0.001];
+function [ perturbed ] = perturb( funparams )
+    perturbed = funparams + (rand(length(funparams), 1) - 0.5) * 0.2;
 end
 
